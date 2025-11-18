828aef295ab3946d16f8e32cc2f5309b9acd3c35
playup5ytb-creator <playup5ytb@gmail.com>
2025-11-18 18:27:49 +0700

    Add files via upload

---
+++ W-Azure.lua
@@ -0,0 +1,23 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/hluuvn/Roblox/refs/heads/main/main.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "📜W-Azure",
+         Animation = "👨‍💻By @ken013122"
+         },
+        Key = {
+        KeySystem = true,
+        Title = "🔐Key W-Azure",
+        Description = "",
+        KeyLink = "Dán link getkey vào đây",
+        Keys = {"nhập key mặc định ở đây"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "⏳Đang load Script...",
+       Incorrectkey = "⛔Key không chính xác",
+       CopyKeyLink = "🔗Đã sao chép"
+      }
+    }
+  })
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/hluuvn/BloxFruit/refs/heads/main/W-Azure.lua")))()
